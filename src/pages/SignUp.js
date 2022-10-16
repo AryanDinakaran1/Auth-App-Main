6faaadaac21d2f7cba23d58d61161a2c7bd83453
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-16 16:01:23 +0530

    Built SignUp

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,16 @@
 import React, { useState } from 'react';
 
 function SignUp() {
-
-    const [username, setUsername] = useState();
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [password, setPassword] = useState();
-
     return(
         <div>
 
             <h1>Sign Up</h1>
             <form action="http://127.0.0.1:8000/signup" method="post">
 
-                <input type="text" value={username} name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-                <input type="text" value={firstName} name="first_name" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
-                <input type="text" value={lastName} name="last_name" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
-                <input type="password" value={password} name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <input type="text" value={username} name="username" placeholder="Username" />
+                <input type="text" value={firstName} name="first_name" placeholder="First Name" />
+                <input type="text" value={lastName} name="last_name" placeholder="Last Name" />
+                <input type="password" value={password} name="password" placeholder="Password" />
                 <input type="submit" />
 
             </form>
