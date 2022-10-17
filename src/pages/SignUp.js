4de31be70637d9189e844a7880ab4baf4b61a7ee
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-17 14:05:14 +0530

    Sign Up Done

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SignUp() {
+
     return(
         <div>
 
             <h1>Sign Up</h1>
             <form action="http://127.0.0.1:8000/signup" method="post">
 
-                <input type="text" value={username} name="username" placeholder="Username" />
-                <input type="text" value={firstName} name="first_name" placeholder="First Name" />
-                <input type="text" value={lastName} name="last_name" placeholder="Last Name" />
-                <input type="password" value={password} name="password" placeholder="Password" />
+                <input type="text" name="username" placeholder="Username" />
+                <input type="text" name="first_name" placeholder="First Name" />
+                <input type="text" name="last_name" placeholder="Last Name" />
+                <input type="password" name="password" placeholder="Password" />
                 <input type="submit" />
 
             </form>
