934ab6fdf59ab570dc20788b269fd50d9fad6b40
Aryan Dinakaran <arydinakarangmail.com@Aryans-MacBook-Pro.local>
2022-10-19 00:17:00 +0530

    Firebase authenticaiton is done just have to fix the post method with axios

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,21 +1,57 @@
 import React, { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase-config';
 import axios from 'axios';
 
 function SignUp() {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const register = async () => {
+
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user);
+
+            const newuser = {
+                username: username,
+                email: email,
+                password: password,
+            };
+
+            axios.post('http://127.0.0.1:8000/signup', { newuser })
+            .then((response) => {
+                console.log(response);
+            })
+            .error((response) => {
+                console.log(response)
+            })
+
+        }
+        catch(error) {
+            console.log(error);
+        }
+
+    };
 
     return(
         <div>
 
             <h1>Sign Up</h1>
+            {/*
             <form action="http://127.0.0.1:8000/signup" method="post">
+            */}
 
-                <input type="text" name="username" placeholder="Username" />
-                <input type="text" name="first_name" placeholder="First Name" />
-                <input type="text" name="last_name" placeholder="Last Name" />
-                <input type="password" name="password" placeholder="Password" />
-                <input type="submit" />
+            <input type="text" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={register}>Sign Up</button>
 
+            {/*
             </form>
+            */}
         </div>
     );
 
---
+++ src/pages/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB53D4T48BqFH_HhMD2WtD4h4tgTXXpZE",
+  authDomain: "fir-app-b7de4.firebaseapp.com",
+  projectId: "fir-app-b7de4",
+  storageBucket: "fir-app-b7de4.appspot.com",
+  messagingSenderId: "229859012034",
+  appId: "1:229859012034:web:096f6d7d894b254fa7ba5b",
+  measurementId: "G-67855TX9LX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
